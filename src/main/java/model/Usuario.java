10d2e4b62908730546c3a4fde771284fc4c406c6
Pablo Curty <pablocurty@id.uff.br>
2016-10-23 12:42:59 -0200

    explicacao de atributos de um pra um

--- src/main/java/model/Usuario.java
+++ src/main/java/model/Usuario.java
@@ -2,6 +2,7 @@ package model;
 
 import java.io.Serializable;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
@@ -10,6 +11,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToOne;
 import javax.persistence.OrderBy;
+import javax.persistence.PrimaryKeyJoinColumn;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 
@@ -18,34 +20,58 @@ import javax.persistence.Table;
 //Uma classe login talvez nao seja necessaria, uma vez que isso eh uma consulta ao banco
 
 @Entity
-@Table(name="USUARIO")
-@SequenceGenerator(name="SEQUENCIA_USUARIO",
-				   sequenceName="SEQ_USUARIO",
-				   allocationSize=1)
-public class Usuario implements Serializable{
+@Table(name = "USUARIO")
+@SequenceGenerator(name = "SEQUENCIA_USUARIO", sequenceName = "SEQ_USUARIO", allocationSize = 1)
+public class Usuario implements Serializable {
 
 	private static final long serialVersionUID = 1L;
-		
+
 	/*------------- atributos do objeto ------------- */
-	
+
 	@Id
-	@GeneratedValue(strategy=GenerationType.IDENTITY, generator="SEQUENCIA_USUARIO")
-	@Column(name="ID_USUARIO")
+	@GeneratedValue(strategy = GenerationType.IDENTITY, generator = "SEQUENCIA_USUARIO")
+	@Column(name = "ID_USUARIO")
 	private long id_ususario;
-	@Column(name="NOME_USUARIO", nullable=false)
+	@Column(name = "NOME_USUARIO", nullable = false)
 	private String nome_usuario;
-	@Column(name="EMAIL_USUARIO", nullable=false)
+	@Column(name = "EMAIL_USUARIO", nullable = false)
 	private String email;
-	@Column(name="USERNAME_USUARIO", nullable=false)
+	@Column(name = "USERNAME_USUARIO", nullable = false)
 	private String username;
-	@Column(name="PASSWORD_USUARIO", nullable=false)
+	@Column(name = "PASSWORD_USUARIO", nullable = false)
 	private String password;
-	@OneToOne(fetch=FetchType.LAZY)
-	@OrderBy
+
+	/**
+	 * Sobre os parâmetros da anotação @OneToOne:
+	 *
+	 * “cascade” – define ações automatizadas no relacionamento, ex.: Ao apagar
+	 * um USUÁRIO, apagar também uma AGENDA. 
+	 * 
+	 * “optional” – você não será obrigado a ter uma AGENDA ao persistir um
+	 * USUÁRIO, ou seja, você não tem que criar um AGENDA para satisfazer essa
+	 * condição (você poderá buscar o USUÁRIO no banco de dados, mas
+	 * Usuario.getAgenda() terá null como resposta). Com o valor igual a false,
+	 * ao se cadastrar um USUÁRIO, é obrigatória a presença de uma AGENDA. Pode
+	 * ser uma AGENDA recém ainda não persistido. 
+	 * 
+	 * “fetch” – o valor padrão é EAGER.
+	 * Ou seja, ao carregar o USUÁRIO já será feita a consulta relacionada a
+	 * AGENDA de modo automático. 
+	 * 
+	 * “orphanRemoval” – define que uma entidade dependente, caso não tenha
+	 * relacionamento, será removida do banco de dados (em nosso modelo, AGENDA
+	 * depende de USUÁRIO). Caso exista uma AGENDA sem USUARIO, essa AGENDA será
+	 * removida.
+	 * 
+	 * 
+	 */
+
+	@OneToOne(cascade = CascadeType.ALL, optional = true, fetch = FetchType.EAGER, orphanRemoval = true)
+	@PrimaryKeyJoinColumn
 	private Agenda agenda;
 
 	/*------------- construtor ------------- */
-	
+
 	public Usuario(long idUsusario, String nome, String email, String username, String password, Agenda agenda) {
 		super();
 		this.id_ususario = idUsusario;
@@ -55,9 +81,9 @@ public class Usuario implements Serializable{
 		this.password = password;
 		this.agenda = agenda;
 	}
-	
+
 	/*------------- get and set ------------- */
-	
+
 	public long getIdUsusario() {
 		return id_ususario;
 	}
